58473693423c160814a454418495005c1e293ab1
romanxlee <romanxlee@yandex.ru>
2023-08-01 23:28:19 +0300

    lint

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -1,101 +1,127 @@
 import { useState, type FC } from "react";
-import BurgerIngredientsStyles from "./burger-ingredients.module.css"
-import { Tab, CurrencyIcon, Counter } from "@ya.praktikum/react-developer-burger-ui-components"
+import BurgerIngredientsStyles from "./burger-ingredients.module.css";
+import {
+  Tab,
+  CurrencyIcon,
+  Counter,
+} from "@ya.praktikum/react-developer-burger-ui-components";
 import type { Ingredient } from "../../types";
 
-const TABS = ["Булки", "Соусы", "Начинки"]
+const TABS = ["Булки", "Соусы", "Начинки"];
 
 type Props = {
-    ingredients: Ingredient[]
-}
+  ingredients: Ingredient[];
+};
 
-const BurgerIngredients:FC<Props> = (props) => {
-    const [current, setCurrent] = useState("Булки")
+const BurgerIngredients: FC<Props> = (props) => {
+  const [current, setCurrent] = useState("Булки");
 
-    const BUNS = props.ingredients.filter(item => item.type === 'bun')
-    const SAUCES = props.ingredients.filter(item => item.type === 'sauce')
-    const MAINS = props.ingredients.filter(item => item.type === 'main')
+  const BUNS = props.ingredients.filter((item) => item.type === "bun");
+  const SAUCES = props.ingredients.filter((item) => item.type === "sauce");
+  const MAINS = props.ingredients.filter((item) => item.type === "main");
 
-    return (
-        <div>
-            <h3 className="text text_type_main-medium mt-10 mb-5">Соберите бургер</h3>
-            <div className={`${BurgerIngredientsStyles.tabs} mb-10`}>
-                {
-                    TABS.map((tab) => (
-                        <Tab active={current === tab} value={tab} key={tab} onClick={setCurrent}>
-                            {tab}
-                        </Tab>
-                ))}
-            </div>
-            <div className={BurgerIngredientsStyles.scrollable}>
-                <section className={BurgerIngredientsStyles.section}>
-                    <h4 className="text text_type_main-medium">Булки</h4>
-                    <div className={BurgerIngredientsStyles.container}>
-                        {
-                            BUNS.map(bun => (
-                                <div key={bun._id} className={BurgerIngredientsStyles.card}>
-                                    <Counter count={1} size="default" extraClass="m-1"/>
-                                    <img src={bun.image} alt={`Изображение ${bun.name}`}
-                                         className={BurgerIngredientsStyles.cardImage}/>
-                                    <div className={BurgerIngredientsStyles.price}>
-                                        <span className="text text_type_digits-default mr-2">{bun.price}</span>
-                                        <CurrencyIcon type="primary"/>
-                                    </div>
-                                    <span key={bun._id}
-                                          className={`${BurgerIngredientsStyles.name} text text_type_main-small`}>{bun.name}</span>
-                                </div>
+  return (
+    <div>
+      <h3 className="text text_type_main-medium mt-10 mb-5">Соберите бургер</h3>
+      <div className={`${BurgerIngredientsStyles.tabs} mb-10`}>
+        {TABS.map((tab) => (
+          <Tab
+            active={current === tab}
+            value={tab}
+            key={tab}
+            onClick={setCurrent}
+          >
+            {tab}
+          </Tab>
+        ))}
+      </div>
+      <div className={BurgerIngredientsStyles.scrollable}>
+        <section className={BurgerIngredientsStyles.section}>
+          <h4 className="text text_type_main-medium">Булки</h4>
+          <div className={BurgerIngredientsStyles.container}>
+            {BUNS.map((bun) => (
+              <div key={bun._id} className={BurgerIngredientsStyles.card}>
+                <Counter count={1} size="default" extraClass="m-1" />
+                <img
+                  src={bun.image}
+                  alt={`Изображение ${bun.name}`}
+                  className={BurgerIngredientsStyles.cardImage}
+                />
+                <div className={BurgerIngredientsStyles.price}>
+                  <span className="text text_type_digits-default mr-2">
+                    {bun.price}
+                  </span>
+                  <CurrencyIcon type="primary" />
+                </div>
+                <span
+                  key={bun._id}
+                  className={`${BurgerIngredientsStyles.name} text text_type_main-small`}
+                >
+                  {bun.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </section>
 
-                            ))
-                        }
-                    </div>
-                </section>
+        <section className={BurgerIngredientsStyles.section}>
+          <h4 className="text text_type_main-medium">Соусы</h4>
+          <div className={BurgerIngredientsStyles.container}>
+            {SAUCES.map((sauce) => (
+              <div key={sauce._id} className={BurgerIngredientsStyles.card}>
+                <Counter count={1} size="default" extraClass="m-1" />
+                <img
+                  src={sauce.image}
+                  alt={`Изображение ${sauce.name}`}
+                  className={BurgerIngredientsStyles.cardImage}
+                />
+                <div className={BurgerIngredientsStyles.price}>
+                  <span className="text text_type_digits-default mr-2">
+                    {sauce.price}
+                  </span>
+                  <CurrencyIcon type="primary" />
+                </div>
+                <span
+                  key={sauce._id}
+                  className={`${BurgerIngredientsStyles.name} text text_type_main-small`}
+                >
+                  {sauce.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </section>
 
-                <section className={BurgerIngredientsStyles.section}>
-                    <h4 className="text text_type_main-medium">Соусы</h4>
-                    <div className={BurgerIngredientsStyles.container}>
-                        {
-                            SAUCES.map(sauce => (
-                                <div key={sauce._id} className={BurgerIngredientsStyles.card}>
-                                    <Counter count={1} size="default" extraClass="m-1"/>
-                                    <img src={sauce.image} alt={`Изображение ${sauce.name}`}
-                                         className={BurgerIngredientsStyles.cardImage}/>
-                                    <div className={BurgerIngredientsStyles.price}>
-                                        <span className="text text_type_digits-default mr-2">{sauce.price}</span>
-                                        <CurrencyIcon type="primary"/>
-                                    </div>
-                                    <span key={sauce._id}
-                                          className={`${BurgerIngredientsStyles.name} text text_type_main-small`}>{sauce.name}</span>
-                                </div>
+        <section className={BurgerIngredientsStyles.section}>
+          <h4 className="text text_type_main-medium">Начинки</h4>
+          <div className={BurgerIngredientsStyles.container}>
+            {MAINS.map((main) => (
+              <div key={main._id} className={BurgerIngredientsStyles.card}>
+                <Counter count={1} size="default" extraClass="m-1" />
+                <img
+                  src={main.image}
+                  alt={`Изображение ${main.name}`}
+                  className={BurgerIngredientsStyles.cardImage}
+                />
+                <div className={BurgerIngredientsStyles.price}>
+                  <span className="text text_type_digits-default mr-2">
+                    {main.price}
+                  </span>
+                  <CurrencyIcon type="primary" />
+                </div>
+                <span
+                  key={main._id}
+                  className={`${BurgerIngredientsStyles.name} text text_type_main-small`}
+                >
+                  {main.name}
+                </span>
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
 
-                            ))
-                        }
-                    </div>
-                </section>
-
-                <section className={BurgerIngredientsStyles.section}>
-                    <h4 className="text text_type_main-medium">Начинки</h4>
-                    <div className={BurgerIngredientsStyles.container}>
-                        {
-                            MAINS.map(main => (
-                                <div key={main._id} className={BurgerIngredientsStyles.card}>
-                                    <Counter count={1} size="default" extraClass="m-1"/>
-                                    <img src={main.image} alt={`Изображение ${main.name}`}
-                                         className={BurgerIngredientsStyles.cardImage}/>
-                                    <div className={BurgerIngredientsStyles.price}>
-                                        <span className="text text_type_digits-default mr-2">{main.price}</span>
-                                        <CurrencyIcon type="primary"/>
-                                    </div>
-                                    <span key={main._id}
-                                          className={`${BurgerIngredientsStyles.name} text text_type_main-small`}>{main.name}</span>
-                                </div>
-
-                            ))
-                        }
-                    </div>
-                </section>
-            </div>
-        </div>
-    )
-}
-
-export default BurgerIngredients
+export default BurgerIngredients;
