98332d5a8d8a1cf2d500f9672db6bcfa53b143e0
Roman Lee <romanxlee@yandex.ru>
2023-11-03 17:30:09 +0300

    fix header

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -6,20 +6,42 @@ import {
   ProfileIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const AppHeader = () => {
+  const location = useLocation();
   return (
     <header className={`${AppHeaderStyles.header} p-4`}>
       <div
         className={`${AppHeaderStyles.container} ${AppHeaderStyles.flexBasis}`}
       >
         <Link to="/" className={AppHeaderStyles.link}>
-          <BurgerIcon type="primary" />
-          <span className="text text_type_main-default m-2">Конструктор</span>
+          <BurgerIcon
+            type={location.pathname === "/" ? "primary" : "secondary"}
+          />
+          <span
+            className={`${
+              location.pathname === "/"
+                ? "text_color_primary"
+                : "text_color_inactive"
+            } text text_type_main-default m-2`}
+          >
+            Конструктор
+          </span>
         </Link>
         <Link to="/profile/orders" className={AppHeaderStyles.link}>
-          <ListIcon type="secondary" />
-          <span className="text text_type_main-default text_color_inactive m-2">
+          <ListIcon
+            type={
+              location.pathname === "/profile/orders" ? "primary" : "secondary"
+            }
+          />
+          <span
+            className={`${
+              location.pathname === "/profile/orders"
+                ? "text_color_primary"
+                : "text_color_inactive"
+            } text text_type_main-default m-2`}
+          >
             Лента заказов
           </span>
         </Link>
@@ -34,8 +56,16 @@ const AppHeader = () => {
         to="/profile"
         className={`${AppHeaderStyles.link} ${AppHeaderStyles.flexBasis} ${AppHeaderStyles.lastLink}`}
       >
-        <ProfileIcon type="secondary" />
-        <span className="text text_type_main-default text_color_inactive m-2">
+        <ProfileIcon
+          type={location.pathname === "/profile" ? "primary" : "secondary"}
+        />
+        <span
+          className={`${
+            location.pathname === "/profile"
+              ? "text_color_primary"
+              : "text_color_inactive"
+          } text text_type_main-default m-2`}
+        >
           Личный кабинет
         </span>
       </Link>
